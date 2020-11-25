083eee0c001233db1ea2cb61215577779b645664
NathyF <moura.nathalia99@gmail.com>
2020-11-24 22:31:51 -0300

    adiciona mapa e base da função de carrosel

---
+++ public/scripts/page-orphanage.js
@@ -0,0 +1,24 @@
+const options = {
+  dragging: false,
+  touchZoom: false,
+  doubleClickZoom: false,
+  scrollWheelZoom: false,
+  zoomControl: false,
+};
+
+const map = L.map('mapid', options).setView([-23.3211063, -51.2358038], 16);
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
+
+const icon = L.icon({
+  iconUrl: './public/images/map-marker.svg',
+  iconSize: [58, 68],
+  iconAnchor: [29, 68],
+  popupAnchor: [170, 2],
+});
+
+L.marker([-23.3211063, -51.2358038], { icon }).addTo(map);
+
+function selectImage(event) {
+  const button = event.currentTarget;
+}
